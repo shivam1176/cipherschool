446dc6aebcfadd0aeb831c057844b878b86c27d0
shivam1176 <jindalshivam009@gmail.com>
2024-07-16 12:26:27 +0530

    first commit

---
+++ class14/examp.js
@@ -0,0 +1,22 @@
+// console.log("my consile statement")
+// var score = 10;
+// let result = 50;
+// const PI = 3.14;
+
+
+// console.log(score);
+
+var score = 10;
+let result = "10";
+const f = score + result ;
+const p = score- result ;
+let totalGames = 5;
+let avg = f/totalGames;
+console.log(f,p);
+console.log(typeof f);
+console.log(typeof p);
+console.log(typeof avg);
+console.log(typeof result);
+let val = f**2 +p *totalGames/avg
+
+console.log(typeof val);
